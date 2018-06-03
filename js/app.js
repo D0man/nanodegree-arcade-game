8c76648bd635da587936e14c13a6ab43d4ff0bf7
d0man <jakub.tomasz.domanski@gmail.com>
2018-06-03 16:30:05 +0200

    done game

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function({x=-100,speed=1} = {}) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = 'images/char-cat-girl.png';
+    this.x= x;
+    this.y= [220,140,60][randomizer(3,0)];
+    this.speed = speed*randomizer(100,50);
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +18,24 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x+= this.speed*dt;
+    if (this.x>0 && Math.abs(this.x-player.x)<10 &&this.y===player.y){
+        player.alive=false;
+        player.sprite= 'images/death-bug.png';
+    }
+    if (this.x-550>0 && player.alive){
+        this.x=-200*randomizer(5,1);
+        if(player.score >= 5 && allEnemies.length < 10){
+        this.speed+= 10*randomizer(5,1);
+        allEnemies.push(new Enemy());
+        console.log(allEnemies);
+        }
+        if(player.score >= 10 && this.speed<1000){
+            this.speed+= 100*randomizer(2,0.5);
+            }
+    }
+    
+    
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,23 +46,77 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(){
+    this.x = 200;
+    this.y = 380;
+    this.sprite = 'images/enemy-bug.png';
+    this.alive = true;
+    this.score = 0;
+    
+}
+Player.prototype.update = function() {
+   
+    if (this.y===-20){
+        this.score++;
+        this.y=380;
+    }
+    
+}
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
+    ctx.fillStyle ='black';
+    if (!this.alive){
+        ctx.fillStyle = 'white';
+        ctx.font="38px serif";
+        ctx.fillText(`You loss! Score:${player.score}`, 120, 110);
+        ctx.fillText(`Press space to play again`, 70, 450);
 
-
+    }
+};
+Player.prototype.handleInput= function(e){
+    if (this.alive){
+        switch(e){
+            case 'up':
+                if ( this.y>0 ) this.y-=80;
+                console.log(this.x)
+                break;
+            case 'down':
+                if ( this.y<380 ) this.y+=80;
+                break;
+            case 'right': 
+                if ( this.x<400 )this.x+=100;
+                break;
+            case 'left':
+            if( this.x>0 ) this.x-=100;
+                break;
+        }
+    }else if (e === 'space'){
+     player = new Player();
+     allEnemies =[new Enemy({speed:4})];
+    }
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let player = new Player();
+let allEnemies =[new Enemy({speed:4})];
 
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keydown', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+        32: 'space'
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+//randoomizer function that get random nuber between max and min
+function randomizer(max,min){
+    return Math.round(Math.random()*max-min)+min;
+}
--- js/engine.js
+++ js/engine.js
@@ -103,6 +103,7 @@ var Engine = (function(global) {
      * they are just drawing the entire screen over and over.
      */
     function render() {
+    
         /* This array holds the relative URL to the image used
          * for that particular row of the game level.
          */
@@ -120,7 +121,8 @@ var Engine = (function(global) {
         
         // Before drawing, clear existing canvas
         ctx.clearRect(0,0,canvas.width,canvas.height)
-
+        ctx.fillText(`Score:${player.score}`, 400, 40);
+        ctx.font="18px serif";
         /* Loop through the number of rows and columns we've defined above
          * and, using the rowImages array, draw the correct image for that
          * portion of the "grid"
@@ -149,11 +151,12 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
+        player.render();
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
 
-        player.render();
+       
     }
 
     /* This function does nothing but it could have been a good place to
@@ -172,8 +175,10 @@ var Engine = (function(global) {
         'images/stone-block.png',
         'images/water-block.png',
         'images/grass-block.png',
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/death-bug.png'
     ]);
     Resources.onReady(init);
 
@@ -183,3 +188,6 @@ var Engine = (function(global) {
      */
     global.ctx = ctx;
 })(this);
+
+
+  
